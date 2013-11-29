7f4bb9c29f10c6b58794690ddba6d0c57e637df8
datasys2 <datasys2@hec.cs.iit.edu>
2013-11-29 12:10:21 -0600

    modifiy the batch

--- src/batch/batch_kill_client.sh
+++ src/batch/batch_kill_client.sh
@@ -1,13 +1,6 @@
-COUNTER=6
-while [ $COUNTER -lt 23 ]; do
-        if [ $COUNTER -ne 15 ]; then
-        if [ $COUNTER -lt 10 ]; then
-                ssh hec-0$COUNTER ./projects/ZHT/kill.sh benchmark_client
-                ssh hec-0$COUNTER ./projects/ZHT/kill.sh benchmark_client
-        else
-                ssh hec-$COUNTER ./projects/ZHT/kill.sh benchmark_client
-                ssh hec-$COUNTER ./projects/ZHT/kill.sh benchmark_client
-        fi
-        fi
-        let COUNTER=COUNTER+1
+COUNTER=21
+while [ $COUNTER -lt 24 ]; do
+	ssh hec-$COUNTER ./projects/ZHT/kill.sh benchmark_client
+	ssh hec-$COUNTER ./projects/ZHT/kill.sh benchmark_client
+	let COUNTER=COUNTER+1
 done
--- src/batch/batch_kill_server.sh
+++ src/batch/batch_kill_server.sh
@@ -1,15 +1,6 @@
-COUNTER=6
+COUNTER=21
 while [ $COUNTER -lt 24 ]; do
-        if [ $COUNTER -ne 10 ]; then
-        if [ $COUNTER -ne 15 ]; then
-        if [ $COUNTER -lt 10 ]; then
-                ssh hec-0$COUNTER ./projects/ZHT/kill.sh server_zht
-                ssh hec-0$COUNTER ./projects/ZHT/kill.sh server_zht
-        else
-                ssh hec-$COUNTER ./projects/ZHT/kill.sh server_zht
-                ssh hec-$COUNTER ./projects/ZHT/kill.sh server_zht
-        fi
-        fi
-        fi
+	ssh hec-$COUNTER ~/zht-eventual-consistency/src/batch/kill.sh server_zht
+	ssh hec-$COUNTER ~/zht-eventual-consistency/src/batch/kill.sh server_zht
         let COUNTER=COUNTER+1
 done
